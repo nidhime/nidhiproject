ed25f8ba8ddd84365753bc6dfedfb998a3991afe
Vishal <Vishal@192.168.1.104>
2018-11-08 11:39:30 +0530

    First Commit

---
+++ nidhiproject/src/helloNidhi/HelloNidhi.java
@@ -0,0 +1,17 @@
+package helloNidhi;
+
+
+public class HelloNidhi {
+	
+	public String checkMessage() {
+		String msg= "Nidhi";
+		return msg;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Hello Nidhi Here");
+	}
+
+}
