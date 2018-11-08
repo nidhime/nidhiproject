646f8f95b2ef2654a77b7130d12ff995d17bc85d
Vishal <Vishal@192.168.1.104>
2018-11-08 13:31:02 +0530

    removed check method

--- nidhiproject/src/helloNidhi/HelloNidhi.java
+++ nidhiproject/src/helloNidhi/HelloNidhi.java
@@ -2,17 +2,15 @@ package helloNidhi;
 
 //First Commit
 //Second Commit
+
+
 public class HelloNidhi {
 	
-	public String checkMessage() {
-		String msg= "Nidhi";
-		return msg;
-		
-	}
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.println("Hello Nidhi Here");
 	}
 
 }
+                  
